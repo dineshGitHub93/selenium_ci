69a6bc1f62aa71776a6c0e900d77801ced860303
dineshGitHub93 <dineshspachaiyappan93@gmail.com>
2025-01-21 04:59:43 +0530

    CI project comment

---
+++ JenkinsCI/src/main/java/com/test/ci/FirstCI.java
@@ -0,0 +1,31 @@
+package com.test.ci;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class FirstCI {
+	
+	public WebDriver driver;
+	
+	  @Test
+	  public void openMyBlog() {
+		driver.get("https://www.softwaretestingmaterial.com/");
+	  }
+	  
+	  @BeforeClass
+	  public void beforeClass() {
+		  
+		  //System.setProperty("webdriver.gecko.driver", "D:\\Selenium\\Drivers\\geckodriver.exe");
+		  driver = new ChromeDriver();
+		  
+	  }
+
+	  @AfterClass
+	  public void afterClass() {
+		  driver.quit();
+	  }
+
+}
